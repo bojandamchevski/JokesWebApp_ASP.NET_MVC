f7e00ba3fe0f8b613f1390a6773ca99991591d36
bojandamchevski <bojandamcevski@gmail.com>
2021-08-16 16:38:39 +0200

    app

---
+++ BojanDamchevski.JokesWebApp/BojanDamchevski.JokesWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BojanDamchevski.JokesWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
